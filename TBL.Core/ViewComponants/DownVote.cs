bca781bb30059c31d2535b87124bede16ae79c13
zeyad <ziadtalaat86@gmail.com>
2025-05-15 22:47:46 +0300

    display number of voting

---
+++ TBL.Core/ViewComponants/DownVote.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TBL.Core.Contracts;
+using TBL.Core.Enums;
+
+namespace TBL.Core.ViewComponants
+{
+    [ViewComponent]
+    public class DownVote : ViewComponent
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public DownVote(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public async Task<IViewComponentResult> InvokeAsync(int productId)
+        {
+
+            var Dislikes = await _unitOfWork.Vote.GetAllAsync(x => x.ProductId == productId && x.VoteType == Voting.DownVote, false);
+
+            int count = Dislikes.Count();
+            return View(count);
+        }
+    }
+}
---
+++ TBL.Core/ViewComponants/UpVote.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using TBL.Core.Contracts;
+using TBL.Core.Enums;
+
+namespace TBL.Core.ViewComponants
+{
+    [ViewComponent]
+    public class UpVote:ViewComponent
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public UpVote(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public async Task<IViewComponentResult> InvokeAsync(int productId)
+        {
+
+           var likes= await _unitOfWork.Vote.GetAllAsync(x => x.ProductId == productId && x.VoteType == Voting.UpVote, false);
+
+            int count=likes.Count();
+            return  View(count);
+        }
+    }
+}
